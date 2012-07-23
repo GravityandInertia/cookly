f2230f10c731177326c6daa589f0399d0dd18d38
Brandon Donnelly <infinit.brandon@gmail.com>
2012-07-23 18:45:12 -0500

    First Commit
    New Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookly::Application.config.secret_token = 'edb934629b739f975ab6ea5e4a10ce63e71ca8ed4810ca95bfe28547066473c52610d74a71e50e66487fb88ab8155d19abb7abd85d2be5c077f450c10d51daac'
